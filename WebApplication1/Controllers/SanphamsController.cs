89394d5333198260f6454f45b98020753a6469bb
IT Supporter <ThomasLe-dev>
2023-06-01 10:11:28 +0700

    Add project files.

---
+++ WebApplication1/Controllers/SanphamsController.cs
@@ -0,0 +1,169 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using WebApplication1.Models;
+using PagedList;
+
+namespace WebApplication1.Controllers
+{
+    public class SanphamsController : Controller
+    {
+        private Model1 db = new Model1();
+
+        // GET: Sanphams
+        public ActionResult Index(string searchValue, string sortOrder, string currentFilter, int? page)
+        {
+            ViewBag.currentSort = sortOrder;
+            ViewBag.sapXepTheoTen = String.IsNullOrEmpty(sortOrder) ? "name_desc" : "";
+            ViewBag.sapXepTheoGia = sortOrder == "Gia" ? "price_desc" : "";
+            if (searchValue != null)
+            {
+                page = 1;
+            }
+            else
+            {
+                searchValue = currentFilter;
+            }
+            ViewBag.currentFilter = searchValue;
+            var sanphams = db.Sanphams.Select(p => p);
+            if (!String.IsNullOrEmpty(searchValue))
+            {
+                if(decimal.TryParse(searchValue, out decimal price))
+                {
+                    sanphams = sanphams.Where(p => p.Giatien == price);
+                }
+                else
+                {
+                    sanphams = sanphams.Where(p => p.Tenvd.Contains(searchValue));
+                }
+            }
+            switch (sortOrder)
+            {
+                case "name_desc":
+                    sanphams = sanphams.OrderByDescending(p => p.Tenvd);
+                    break;
+                case "price_desc":
+                    sanphams = sanphams.OrderByDescending(p => p.Giatien);
+                    break;
+                default: sanphams = sanphams.OrderBy(p => p.Danhmuc.TenDanhmuc);
+                    break;
+            }
+            int pageSize = 4;
+            int pageNumber = (page ?? 1);
+            return View(sanphams.ToPagedList(pageNumber, pageSize));
+        }
+
+        // GET: Sanphams/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Sanpham sanpham = db.Sanphams.Find(id);
+            if (sanpham == null)
+            {
+                return HttpNotFound();
+            }
+            return View(sanpham);
+        }
+
+        // GET: Sanphams/Create
+        public ActionResult Create()
+        {
+            ViewBag.MaDanhmuc = new SelectList(db.Danhmucs, "MaDanhmuc", "TenDanhmuc");
+            return View();
+        }
+
+        // POST: Sanphams/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "Mavd,Tenvd,Mota,TenAnh,Giatien,Soluong,MaDanhmuc")] Sanpham sanpham)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Sanphams.Add(sanpham);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.MaDanhmuc = new SelectList(db.Danhmucs, "MaDanhmuc", "TenDanhmuc", sanpham.MaDanhmuc);
+            return View(sanpham);
+        }
+
+        // GET: Sanphams/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Sanpham sanpham = db.Sanphams.Find(id);
+            if (sanpham == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.MaDanhmuc = new SelectList(db.Danhmucs, "MaDanhmuc", "TenDanhmuc", sanpham.MaDanhmuc);
+            return View(sanpham);
+        }
+
+        // POST: Sanphams/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "Mavd,Tenvd,Mota,TenAnh,Giatien,Soluong,MaDanhmuc")] Sanpham sanpham)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(sanpham).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.MaDanhmuc = new SelectList(db.Danhmucs, "MaDanhmuc", "TenDanhmuc", sanpham.MaDanhmuc);
+            return View(sanpham);
+        }
+
+        // GET: Sanphams/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Sanpham sanpham = db.Sanphams.Find(id);
+            if (sanpham == null)
+            {
+                return HttpNotFound();
+            }
+            return View(sanpham);
+        }
+
+        // POST: Sanphams/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Sanpham sanpham = db.Sanphams.Find(id);
+            db.Sanphams.Remove(sanpham);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
